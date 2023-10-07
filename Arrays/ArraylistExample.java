2f32b2eb45708c521b99154aaba7df612e936a24
Abhay Chauhan <Ab66527@gmail.com>
2023-10-07 20:08:24 +0530

    Add files via upload

--- Arrays/ArraylistExample.java
+++ Arrays/ArraylistExample.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArraylistExample{
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         
         ArrayList<Integer> list = new ArrayList<>(10);
 
-        list.add(67);
-        list.add(6547);
-        list.add(89);
-        list.add(56);
+    
 
-        System.out.println(list.contains(67));
+        // list.add(67);
+        // list.add(6547);
+        // list.add(89);
+        // list.add(56);
 
+        // System.out.println(list.contains(67));
+
+        // System.out.println(list);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+            
+        }
         System.out.println(list);
 
 
